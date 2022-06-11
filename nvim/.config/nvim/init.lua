759a1aa5291ff246c9c770103693c0652a0308e3
Janghan Park <janghan0412@gmail.com>
2022-06-11 19:38:14 +1000

    Add alacritty

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -482,7 +482,7 @@ require("gitsigns").setup({
 
 require("null-ls").setup({
 	sources = {
-		require("null-ls").builtins.formatting.stylua,
+		-- require("null-ls").builtins.formatting.stylua,
 		require("null-ls").builtins.formatting.prettierd,
 	},
 })
